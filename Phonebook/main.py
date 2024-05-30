3fa2b869d066e977af171a8cc6461c933de96607
Mikhail Kuznetsov <enwoldewolf@gmail.com>
2024-05-30 15:36:22 +0300

    add findInBook, add 2 choice

--- Phonebook/main.py
+++ Phonebook/main.py
@@ -11,7 +11,7 @@ def clear():
 def draw():
   print("━" * 25)
 
-#Читаем наш файл в phone_book
+# Функция для чтения файла
 def read_txt(filename):
     phone_book = []
     fields = ['Фамилия', 'Имя', 'Телефон', 'Описание']
@@ -25,19 +25,28 @@ def read_txt(filename):
         return (f"Ошибка: Файл '{filename}' не найден.")
     
 # Функция для вывода красивого справочника
-def print_data(data):
+def print_data(book):
     headers = ["№".ljust(3, ' '), "Фамилия".ljust(10, ' '), "Имя".ljust(10, ' '), "Телефон".ljust(13, ' '), "Описание".ljust(63, ' ')]
     print("-" * 117) # Верхняя граница таблицы
     print(f"| {' | '.join(headers)}   |")
     print("-" * 117) # Разделитель под заголовками
-    for i, record in enumerate(data, 1):
+    for i, record in enumerate(book, 1):
         values = [str(i).ljust(3, ' '), record['Фамилия'].ljust(10, ' '), record['Имя'].ljust(10, ' '), record['Телефон'].ljust(13, ' '), record['Описание'].strip().ljust(65, ' ')]
         print(f"| {' | '.join(values)} |")
     print("-" * 117) # Нижняя граница таблицы
 
 
+# Функция для поиска записи
+def findInBook(where, what):
+    findedEntryOut = []
+    for i in bookInMemory: 
+        if what in i[where]:
+            findedEntryOut=[i]
+    return findedEntryOut
+
+
         
-# Сразу считываем справочник  
+# Стандартно при запуске считываем наш справочник phon.txt в bookInMemory. Далее работаем с bookInMemory
 bookInMemory=read_txt(phonebookName)
 
 
@@ -53,8 +62,7 @@ while menu:
     print("│  Телефонный справочник  │")
     print("├─────────────────────────┤")
     print("│ 1. Открыть справочник   │")
-    print("│ 2. Поиск по фамилии     │")
-    print("│ 3. Поиск по номеру      │")
+    print("│ 2. Поиск по справочнику │")
     print("│ 4. Добавление записей   │")
     print("│ 5. Сохранить и выйти    │")
     print("│ 6. Выбор справочника    │")
@@ -71,15 +79,63 @@ while menu:
       draw()
       print_data(bookInMemory)
       draw()
+      # print(bookInMemory)
+      # draw()
       input("> Нажмите Enter, чтобы продолжить ")
       menu=True
 
 
 
-    elif choice == "2": # 2. Поиск по фамилии
-      pass
-    elif choice == "3": # 3. Поиск по номеру
-      pass
+    elif choice == "2": # 2. Поиск по справочнику
+      menu=False
+      draw()
+      print("┌─────────────────────────┐")
+      print("│  Поиск по справочнику   │")
+      print("├─────────────────────────┤")
+      print("│ 1. Поиск по фамилии     │")     
+      print("│ 2. Поиск по имени       │")
+      print("│ 3. Поиск по номеру      │")     
+      print("└─────────────────────────┘")
+      draw()
+      choice3 = input("# Введите команду > ")
+      if choice3 == "1":
+        entry=input("> Введите фамилию: ")
+        fentry=findInBook('Фамилия', entry)
+        if fentry==[]:
+          print("# Записей не найдено!")
+          input("> Нажмите Enter, чтобы продолжить ")
+          clear()
+          menu=True
+        else:
+          print_data(fentry)        
+          input("> Нажмите Enter, чтобы продолжить ")
+          menu=True
+      elif choice3 == "2":
+        entry=input("> Введите имя: ")
+        fentry=findInBook('Имя', entry)
+        if fentry==[]:
+          print("# Записей не найдено!")
+          input("> Нажмите Enter, чтобы продолжить ")
+          menu=True
+        else:
+          print_data(fentry)        
+          input("> Нажмите Enter, чтобы продолжить ")
+          menu=True
+      elif choice3 == "3":
+        entry=input("> Введите номер: ")
+        fentry=findInBook('Телефон', entry)
+        if fentry==[]:
+          print("# Записей не найдено!")
+          input("> Нажмите Enter, чтобы продолжить ")
+          menu=True
+        else:
+          print_data(fentry)        
+          input("> Нажмите Enter, чтобы продолжить ")
+          menu=True
+
+
+
+
     elif choice == "4": # 4. Добавление записей
       pass
     elif choice == "5": # 5. Сохранить и выйти
